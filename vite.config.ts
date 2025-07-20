58fbc511f119e851e034ed93dfa44ba40ea86050
adityawildan <adityawilwil@gmail.com>
2025-07-20 14:18:30 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // This is important for Vercel.
+    // It ensures that the output directory is compatible with Vercel's expectations.
+    outDir: 'dist',
+  },
+})
